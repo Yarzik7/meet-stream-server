e190c9fb4f33564d93dbd0982a34b0658bdda09c
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-11-22 21:26:47 +0200

    Adding hash password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,13 +5,19 @@ import { LogoutAuthDto } from './dto/logout-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './models/user.schema';
 import { Model } from 'mongoose';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async register(registerAuthDto: RegisterAuthDto): Promise<User> {
-    const registeredUser = new this.userModel(registerAuthDto);
+    const hashedPassword = await hash(registerAuthDto.password, 12);
+    console.log(hashedPassword);
+    const registeredUser = new this.userModel({
+      ...registerAuthDto,
+      password: hashedPassword,
+    });
     return registeredUser.save();
   }
 
