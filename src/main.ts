2f921b603476df7e1b44099857a8b38e4e4998b6
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-11-29 13:13:44 +0200

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   await app.listen(process.env.PORT ?? 3001, (): void => {
