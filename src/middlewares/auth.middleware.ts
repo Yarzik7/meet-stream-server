509dd29e2b4d981f64a184ec03c0d8c01dd356fa
Yaroslav Chaplinskyi <116737021+Yarzik7@users.noreply.github.com>
2024-12-06 15:54:26 +0200

    Fix user response in middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -44,8 +44,13 @@ export class AuthMiddleware implements NestMiddleware {
         verify(fetchedUser.refreshToken, REFRESH_TOKEN_SECRET);
         const { accessToken, refreshToken } = await assignToken(fetchedUser);
 
-        fetchedUser = await this.userModel.findByIdAndUpdate(fetchedUser._id, { refreshToken });
-        res.json({ user: fetchedUser, accessToken });
+        fetchedUser = await this.userModel.findByIdAndUpdate(fetchedUser._id, { refreshToken }, {new: true});
+        const userResponse = fetchedUser.toObject();
+
+        delete userResponse.password;
+        delete userResponse.refreshToken;
+
+        res.json({ user: userResponse, accessToken });
       } catch (_) {
         return next(new UnauthorizedException('Refresh token is expired'));
       }
